57add5c0c4e7867c33c6f4ea7e03879620407dce
ORenox <renato1234567r89@gmail.com>
2026-09-21 16:06:31 -0500

    feat:permisions IsAdminOrReadOnly to machines

--- apps/machines/views.py
+++ apps/machines/views.py
@@ -3,6 +3,8 @@ from rest_framework import viewsets
 from apps.machines.models import Machine
 from apps.machines.serializers import MachineSerializer
 
+from apps.permissions import IsAdminOrReadOnly
+
 
 #JWT
 from rest_framework.permissions import IsAuthenticated
@@ -16,4 +18,4 @@ class MachineViewSet(viewsets.ModelViewSet):
     queryset = Machine.objects.all()
     serializer_class = MachineSerializer
 
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAdminOrReadOnly]
