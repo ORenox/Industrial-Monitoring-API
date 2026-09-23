9d0c33f1ac1b1ff1330891758280707ec49eea90
ORenox <renato1234567r89@gmail.com>
2026-09-23 16:44:55 -0500

    docs: finalize project documentation and production config

--- config/settings.py
+++ config/settings.py
@@ -28,9 +28,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv(
+    "DJANGO_ALLOWED_HOSTS",
+    "localhost,127.0.0.1"
+).split(",")
 
 
 #JWT
